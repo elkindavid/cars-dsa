c6fc7751c0938fb9a994a54708358be5fc91a797
david <ed.penar1@uniandes.edu.co>
2023-11-22 23:16:26 -0500

    Deployment API

---
+++ deployment/api.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import joblib
+from model_dep import predict
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes and origins
+
+# Definición aplicación Flask
+app = Flask(__name__)
+
+# Definición API Flask
+api = Api(
+    app, 
+    version='1.0', 
+    title='Car Prices API',
+    description='Car Prices API')
+
+ns = api.namespace('Predict', 
+     description='Car Prices API')
+
+# Definición argumentos o parámetros de la API
+parser = api.parser()
+
+parser.add_argument(
+    'YEAR', 
+    type=int, 
+    required=True, 
+    help='Year', 
+    location='args')
+
+parser.add_argument(
+    'MILEAGE', 
+    type=int, 
+    required=True, 
+    help='Mileage', 
+    location='args')
+
+parser.add_argument(
+    'STATE', 
+    type=str, 
+    required=True, 
+    help='State', 
+    location='args')
+
+parser.add_argument(
+    'MAKE', 
+    type=str, 
+    required=True, 
+    help='Make', 
+    location='args')
+
+parser.add_argument(
+    'MODEL', 
+    type=str, 
+    required=True, 
+    help='Model', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+# Definición de la clase para disponibilización
+@ns.route('/')
+class CarPriceApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict(args['YEAR'], args['MILEAGE'], args['STATE'], args['MAKE'], args['MODEL'])
+        }, 200
+    
+    
+if __name__ == '__main__':
+    # Ejecución de la aplicación que disponibiliza el modelo de manera local en el puerto 5000
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
---
+++ deployment/model.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+import json
+
+def predict(year, mileage, state, make, model):
+    
+    reg = joblib.load(os.path.dirname(__file__) + '/model.pkl') 
+    
+    # Cargar el diccionario desde el archivo JSON
+    with open("deployment/cod_categoricas.json", 'r') as archivo:
+        dic_categoricas = json.load(archivo)
+
+    try:
+        state_ = dic_categoricas['State'][state]
+        make_ = dic_categoricas['Make'][make]
+        model_ = dic_categoricas['Model'][model]
+    except KeyError:
+        state_ = -1
+        make_ = -1
+        model_ = -1
+    
+    car_ = pd.DataFrame([[year, mileage, state_, make_, model_]], columns=['Year','Mileage','State','Make','Model'])   
+    car_['YxM'] = (year * mileage)
+    
+    #Make prediction
+    p1 = int(reg.predict(car_)[0])
+
+    #Rango aceptable de precios +/- 10% del precio de referencia
+    min_price = p1 * 0.90
+    max_price = p1 * 1.10
+
+    similar_cars = []
+    for mk in dic_categoricas['Make'].keys():
+
+        car_2 = pd.DataFrame([[year, mileage, state_, dic_categoricas['Make'][mk], model_]], columns=['Year','Mileage','State','Make','Model'])   
+        car_2['YxM'] = (year * mileage)
+
+        price = int(reg.predict(car_2)[0])
+
+        if min_price <= price <= max_price and mk != make:
+            similar_cars.append((mk,price))
+
+    # Ordena los resultados por similitud de precio
+    similar_cars.sort(key=lambda x: abs(x[1] - p1))
+
+    # Presenta los 5 mejores resultados
+    top_5_similar_cars = similar_cars[:5]
+
+    result_dict = {
+        'Predict': p1,
+        'Top5': top_5_similar_cars
+    }
+
+    return result_dict
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add an URL')
+    else:
+        
+        year = sys.argv[1]
+        mileage = sys.argv[2]
+        state = sys.argv[3]
+        make = sys.argv[4]
+        model = sys.argv[5]
+        
+        p1 = predict(year, mileage, state, make, model)
+        
+        print(car_)
+        print('Car Price: ', p1)
+        
