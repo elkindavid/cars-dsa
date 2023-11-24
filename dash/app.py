60246ab0f3148cedd66d4336a60bfdd39b15220f
segurao <g.segurao@uniandes.edu.co>
2023-11-24 01:14:50 -0500

    Filtros dinamicos dash

--- dash/app.py
+++ dash/app.py
@@ -13,6 +13,7 @@ api_url = "http://44.206.239.130:6500/Api/Predict"
 
 # Importando datos
 df = pd.read_csv('../datos/dataTrain_carListings.csv')
+df_line = df.groupby(['Year','Make'])['Price'].mean().reset_index()
 
 with open('feature_importance.json') as f:
     feature_importance_data = json.load(f)
@@ -27,9 +28,12 @@ with open('Comparativo.json') as f:
     price_data = json.load(f)
 
 # Extraer los datos de año, precio predicho y precio real
-years = [entry['Year'] for entry in price_data]
-predicted_prices = [entry['PredictedPrice'] for entry in price_data]
-real_prices = [entry['RealPrice'] for entry in price_data]
+#years = [entry['Year'] for entry in price_data]
+#predicted_prices = [entry['PredictedPrice'] for entry in price_data]
+#real_prices = [entry['RealPrice'] for entry in price_data]
+
+
+
 
 # Extraer los datos de precio actual y precio anterior
 current_price = price_comparison_data['CurrentPrice']
@@ -184,8 +188,8 @@ app.layout = dbc.Container([
                 figure={
                     'data': [
                         go.Bar(
-                            x=['Current Price', 'Previous Price'],
-                            y=[current_price, previous_price]
+                            x=['Price1', 'Price'],
+                            y=[df_line['Price'], df_line['Price']]
                         )
                     ],
                     'layout': go.Layout(
@@ -222,20 +226,20 @@ app.layout = dbc.Container([
                 figure={
                     'data': [
                         go.Scatter(
-                            x=years,
-                            y=predicted_prices,
+                            x=df_line['Year'],
+                            y=df_line['Price'],
                             mode='lines',
                             name='Predicted Price'
                         ),
                         go.Scatter(
-                            x=years,
-                            y=real_prices,
+                            x=df_line['Year'],
+                            y=df_line['Price'],
                             mode='lines',
                             name='Real Price'
                         )
                     ],
                     'layout': go.Layout(
-                        title='Predicted vs Real Prices Over Years',
+                        title='Predicted vs Real avg Prices  Over Years',
                         xaxis={'title': 'Year'},
                         yaxis={'title': 'Price'}
                     )
@@ -272,6 +276,7 @@ def update_graph(col_chosen):
     fig.update_layout(title_text="Data Distribution")
     return fig
 
+
 @callback(
     Output(component_id='large-text-input', component_property='value'),
     [Input(component_id='predict-button', component_property='n_clicks'),
@@ -307,6 +312,80 @@ def make_api_request(nclicks,year, mileage, state, make, model):
         return "Enter valid data before making the API request"
 
     return resultado
+
+@app.callback(
+    Output(component_id='price-comparison-bar-chart', component_property='figure'),
+    [Input(component_id='make-dropdown', component_property='value'),
+     Input(component_id='year-dropdown', component_property='value')],
+)
+def update_price_comparison_chart(selected_make, selected_year):
+    # Filter data based on the selected make and year
+    filtered_data = df_line[(df_line['Make'] == selected_make) & (df_line['Year'] == selected_year)]
+
+    # Extract the updated values for price
+    updated_prices = filtered_data['Price']
+
+    # Create a new figure for the bar chart
+    fig = {
+        'data': [
+            go.Bar(
+                x=['Price'],
+                y=[updated_prices.mean()] if not updated_prices.empty else [0],
+                text=[f'Make: {selected_make}, Year: {selected_year}, ' if not updated_prices.empty else 'No Data'],
+                hoverinfo='text'
+            )
+        ],
+        'layout': go.Layout(
+            title=f'Avg Price for Make: {selected_make}, Year: {selected_year}',
+            yaxis={'title': 'Average Price'}
+        )
+    }
+
+    return fig
+
+@app.callback(
+    Output(component_id='price-comparison-line-chart', component_property='figure'),
+    [Input(component_id='make-dropdown', component_property='value')],
+)
+def update_price_comparison_chart(selected_make):
+    # Filter data based on the selected make
+    filtered_data = df_line[df_line['Make'] == selected_make]
+
+    # Extract the updated values for years, predicted prices, and real prices
+    updated_years = filtered_data['Year']
+    updated_predicted_prices = filtered_data['Price']
+    updated_real_prices = filtered_data['Price']
+
+    # Create a new figure for the line chart
+    fig = {
+        'data': [
+            go.Scatter(
+                x=updated_years,
+                y=updated_predicted_prices,
+                mode='lines+markers',  # Agrega marcadores a la línea
+                name='Predicted Price',
+                text=[f'Make: {make}, Predicted Price: {predicted_price}' for make, predicted_price in zip([selected_make] * len(updated_years), updated_predicted_prices)],
+                hoverinfo='text'  # Muestra información al pasar el ratón sobre los marcadores
+            ),
+            go.Scatter(
+                x=updated_years,
+                y=updated_real_prices,
+                mode='lines+markers',  # Agrega marcadores a la línea
+                name='Real Price',
+                text=[f'Make: {make}, Real Price: {real_price}' for make, real_price in zip([selected_make] * len(updated_years), updated_real_prices)],
+                hoverinfo='text'  # Muestra información al pasar el ratón sobre los marcadores
+            )
+        ],
+        'layout': go.Layout(
+            title=f'Predicted vs Real Prices for Make: {selected_make}',
+            xaxis={'title': 'Year'},
+            yaxis={'title': 'Price'}
+        )
+    }
+
+    return fig
+
+
 
 # Run the app
 if __name__ == '__main__':
